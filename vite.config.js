1815e30e65d7a9843fc5297510ac68b1f09ed8ff
tanyf5 <leehmann05@gmail.com>
2024-07-14 03:55:39 +0200

    hw10-2

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-js-hw-10/',
     build: {
       sourcemap: true,
 
